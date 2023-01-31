cf617f9df21f1c7163bd65d1dcc441ab2f30d71e
Luiz Filipe <email.luizfilipe@gmail.com>
2023-01-31 20:16:34 -0300

    conclusion of project

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
     //conteudo js/jquery
+    $("#cpf").mask("000.000.000-00")
+    $("#cep").mask("00000-000")
+    $("#telefone").mask("(00) 00000-0000")
+
+    $("form").validate({
+        rules: {
+            nome: {required: true},
+            email: {required: true, email: true},
+            telefone: {required: true},
+            cpf: {required: true},
+            endereco: {required: true},
+            cep: {required: true}
+        }
+    })
 })
